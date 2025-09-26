12f827b493c61f2cb3de356159e4f8344efa99b6
Jeff Peterson <gpeterso@umn.edu>
2025-09-26 16:47:46 -0500

    Add full display selector
    Taken from https://github.com/ExLibrisGroup/customModuleExamples/blob/main/utils/fullDisplayRecordSelector.ts

---
+++ full-display.selector.ts
@@ -0,0 +1,23 @@
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+
+interface FullDisplayState {
+  selectedRecordId: string | null;
+}
+
+interface SearchState {
+  entities: { [key: string]: any };
+}
+
+const selectFullDisplay =
+  createFeatureSelector<FullDisplayState>("full-display");
+const selectSearchState = createFeatureSelector<SearchState>("Search");
+const selectFullDisplayRecordId = createSelector(
+  selectFullDisplay,
+  (fullDisplay: FullDisplayState) => fullDisplay?.selectedRecordId ?? null
+);
+export const selectFullDisplayRecord = createSelector(
+  selectFullDisplayRecordId,
+  selectSearchState,
+  (recordId: string | null, searchState: SearchState) =>
+    recordId ? searchState.entities[recordId] : null
+);
